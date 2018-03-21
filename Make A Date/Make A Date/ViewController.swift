d8c363eed00d73341ebddba5c629dc4e2c2c941e
sakadalim <sakadalim@gmail.com>
2018-03-21 19:19:26 -0400

    Initial Setup
    added AWS pod

---
+++ Make A Date/Make A Date/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Make A Date
+//
+//  Created by Sakada Lim on 3/21/18.
+//  Copyright © 2018 Sakada Lim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Make A Date/Pods/Target Support Files/Pods-Make A Date/Pods-Make A Date-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Make_A_Date : NSObject
+@end
+@implementation PodsDummy_Pods_Make_A_Date
+@end
