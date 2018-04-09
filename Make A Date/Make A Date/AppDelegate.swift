02e1406331fe3efa81cf532c0d8fcbdbc3a97db1
sakadalim <sakadalim@gmail.com>
2018-04-09 16:01:44 -0400

    Removed Testing Code

--- Make A Date/Make A Date/AppDelegate.swift
+++ Make A Date/Make A Date/AppDelegate.swift
@@ -22,8 +22,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        AWSDDLog.add(AWSDDTTYLogger.sharedInstance)
-        AWSDDLog.sharedInstance.logLevel = .info
+//        AWSDDLog.add(AWSDDTTYLogger.sharedInstance)
+//        AWSDDLog.sharedInstance.logLevel = .info
         
         pinpoint = AWSPinpoint(configuration:
             AWSPinpointConfiguration.defaultPinpointConfiguration(launchOptions: launchOptions))
--- Make A Date/Make A Date/ViewControllers/MainViewController.swift
+++ Make A Date/Make A Date/ViewControllers/MainViewController.swift
@@ -20,23 +20,9 @@ class MainViewController: UIViewController {
         
         // Get the AWSCredentialsProvider from the AWSMobileClient
         let credentialsProvider = AWSMobileClient.sharedInstance().getCredentialsProvider()
-        
         // Get the identity Id from the AWSIdentityManager
         let identityId = AWSIdentityManager.default().identityId
         
-        
-//        if !AWSSignInManager.sharedInstance().isLoggedIn {
-//            AWSAuthUIViewController
-//                .presentViewController(with: self.navigationController!,
-//                                       configuration: nil,
-//                                       completionHandler: { (provider: AWSSignInProvider, error: Error?) in
-//                                        if error != nil {
-//                                            print("Error occurred: \(String(describing: error))")
-//                                        } else {
-//                                            // Sign in successful.
-//                                        }
-//                })
-//        }
         if !AWSSignInManager.sharedInstance().isLoggedIn {
             presentAuthUIViewController()
         }
@@ -45,13 +31,10 @@ class MainViewController: UIViewController {
     func presentAuthUIViewController() {
         let config = AWSAuthUIConfiguration()
         config.enableUserPoolsUI = true
-//        config.addSignInButtonView(class: AWSFacebookSignInButton.self)
-//        config.addSignInButtonView(class: AWSGoogleSignInButton.self)
         config.backgroundColor = UIColor.jewel
         config.font = UIFont (name: "Helvetica Neue", size: 15)
         config.isBackgroundColorFullScreen = false
         config.canCancel = true
-//        config.logoImage =
         AWSAuthUIViewController.presentViewController(
             with: self.navigationController!,
             configuration: config, completionHandler: { (provider: AWSSignInProvider, error: Error?) in
